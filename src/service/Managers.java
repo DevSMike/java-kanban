cbfec27c9d84a6bd727d4bcb22f6614ac8fa7d6f
DevSMike <likashev.migha@yandex.ru>
2022-11-16 22:59:16 +0300

    feat: Добавление метода в утилитарный класс для создания базового менеджера истории

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -5,5 +5,9 @@ public class Managers {
    public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
+
+    public static HistoryManager getDefaultHistory() {
+       return new InMemoryHistoryManager();
+    }
 }
 
