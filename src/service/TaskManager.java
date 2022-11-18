8e0b287a2c09fb1cac2f21bc0ca55a1fb8acc22c
DevSMike <likashev.migha@yandex.ru>
2022-11-18 16:38:01 +0300

    fix: добавить абзацы между методами

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -7,30 +7,51 @@ import model.Task;
 import java.util.ArrayList;
 
 public interface TaskManager {
+
      void addNewTaskItem(Task task);
+
      void addNewEpicItem(Epic epic) ;
+
      void addNewSubtaskItem(Subtask subtask);
+
      ArrayList<Task> getTasks();
+
      ArrayList<Epic> getEpics();
+
      ArrayList<Subtask> getSubtasks();
 
      void deleteTasks();
+
      void deleteEpics();
 
      void deleteSubtasks();
+
      Task getTaskById(int id);
+
      Epic getEpicById(int id);
+
      Subtask getSubtaskById(int id);
+
      void updateEpicStatus(Epic epic);
+
      void deleteTaskById(int id);
+
      void deleteEpicById(int id);
+
      void deleteSubtaskById(int id);
+
      ArrayList<Subtask> getSubtaskForEpic(Epic epic);
+
      void updateTask(Task task);
+
      void updateEpic(Epic epic);
+
      void updateSubtask(Subtask subtask);
+
      void setTaskStatus(StatusManager.Statuses status, Task task);
+
      void setSubtaskStatus(StatusManager.Statuses status, Subtask subtask);
+
      HistoryManager getHistoryManager();
 
 }
