838fbfdd5904a050384348d7a8918d980b29e036
DevSMike <likashev.migha@yandex.ru>
2022-12-08 00:45:42 +0300

    feat: добавить сигнатуру метода remove(int id)

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -4,9 +4,11 @@ import model.Task;
 
 import java.util.ArrayList;
 
-public interface HistoryManager {
+public interface HistoryManager  {
 
     void add(Task task);
     ArrayList<Task> getHistory();
+    void remove(int id);
+
 
 }
