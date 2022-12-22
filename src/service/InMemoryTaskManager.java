6267ef6ebfa940807a03dd08a5a6957090adc3e7
DevSMike <likashev.migha@yandex.ru>
2022-12-22 13:37:06 +0300

    fix: приватные поля класса сделать protected, чтобы класс FileBacked мог получить к ним доступ

--- src/service/InMemoryTaskManager.java
+++ src/service/InMemoryTaskManager.java
@@ -9,11 +9,11 @@ import java.util.List;
 
 public class InMemoryTaskManager implements TaskManager {
 
-    private HashMap<Integer, Task> tasks = new HashMap<>();
-    private HashMap<Integer, Epic> epics = new HashMap<>();
-    private HashMap<Integer, Subtask> subtasks = new HashMap<>();
+    protected HashMap<Integer, Task> tasks = new HashMap<>();
+    protected HashMap<Integer, Epic> epics = new HashMap<>();
+    protected HashMap<Integer, Subtask> subtasks = new HashMap<>();
 
-    private  HistoryManager historyManager = Managers.getDefaultHistory();
+    protected  HistoryManager historyManager = Managers.getDefaultHistory();
     protected int nextId = 1;
 
     @Override
