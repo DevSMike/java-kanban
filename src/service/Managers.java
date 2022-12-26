b49cc38a759ebe015e4ef8692c0525f31f8dde64
DevSMike <likashev.migha@yandex.ru>
2022-12-26 21:37:01 +0300

    feat: изменить дефолтный менеджер

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,9 +1,13 @@
 package service;
 
+import java.io.File;
+
 public class Managers {
 
-   public static TaskManager getDefault() {
-        return new InMemoryTaskManager();
+    private static final String PATH = "src/resources/tasksData.csv";
+
+    public static TaskManager getDefault() {
+        return new FileBackedTasksManager(new File(PATH));
     }
 
     public static HistoryManager getDefaultHistory() {
