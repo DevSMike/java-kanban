7657d7de0484cab948bbbca032102c2a21987416
DevSMike <likashev.migha@yandex.ru>
2022-12-26 21:37:45 +0300

    feat: добавить создание менеджера задач через ультимативный класс

--- src/MainForFiles.java
+++ src/MainForFiles.java
@@ -15,45 +15,44 @@ public class MainForFiles {
         File file = new File(PATH);
         File file2 = new File ("src/resources/tasksData2.csv");
 
-        FileBackedTasksManager fbManager = new FileBackedTasksManager(file);
-        Main.printAllTasksInfo(fbManager);
-        Printer.printTaskHistory(fbManager.getHistory());
+        TaskManager taskManager = Managers.getDefault();
+
+        Main.printAllTasksInfo(taskManager);
+        Printer.printTaskHistory(taskManager.getHistory());
 
         Task taskN1 = new Task("Прогулка", "Сходить в лес");
         Task taskN2 = new Task("Почитать", "Чтение перед сном");
-        fbManager.addNewTaskItem(taskN1);
-        fbManager.addNewTaskItem(taskN2);
+        taskManager.addNewTaskItem(taskN1);
+        taskManager.addNewTaskItem(taskN2);
 
         Epic epicN1 = new Epic("Купить подарки на НГ", "Составить список");
         Subtask subtaskEpicN1N1 = new Subtask("Купить коробку", "Заказать на Яндекс Маркете");
         Subtask subtaskEpicN1N2 = new Subtask("Купить ленточку","Заказать на Яндекс Маркете");
-        fbManager.addNewEpicItem(epicN1);
+        taskManager.addNewEpicItem(epicN1);
         subtaskEpicN1N1.setEpicId(epicN1.getId());
         subtaskEpicN1N2.setEpicId(epicN1.getId());
-        fbManager.addNewSubtaskItem(subtaskEpicN1N1);
-        fbManager.addNewSubtaskItem(subtaskEpicN1N2);
+        taskManager.addNewSubtaskItem(subtaskEpicN1N1);
+        taskManager.addNewSubtaskItem(subtaskEpicN1N2);
 
         // ВЫЗЫВАЕМ ИСТОРИЮ ПРОСМОТРА
-        fbManager.getEpicById(epicN1.getId());
-        fbManager.getTaskById(taskN1.getId());
-        fbManager.getTaskById(taskN2.getId());
-        fbManager.getTaskById(taskN1.getId());
-        fbManager.getSubtaskById(subtaskEpicN1N1.getId());
-        fbManager.getSubtaskById(subtaskEpicN1N2.getId());
+        taskManager.getEpicById(epicN1.getId());
+        taskManager.getTaskById(taskN1.getId());
+        taskManager.getTaskById(taskN2.getId());
+        taskManager.getTaskById(taskN1.getId());
+        taskManager.getSubtaskById(subtaskEpicN1N1.getId());
+        taskManager.getSubtaskById(subtaskEpicN1N2.getId());
 
 
         System.out.println();
-        Main.printAllTasksInfo(fbManager);
-        Printer.printTaskHistory(fbManager.getHistory());
+        Main.printAllTasksInfo(taskManager);
+        Printer.printTaskHistory(taskManager.getHistory());
 
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println("Создаеём новый менеджер по данным старого!");
-        FileBackedTasksManager newFb = new FileBackedTasksManager(file);
-        try {
-            newFb = FileBackedTasksManager.loadFromFile(file);
-        } catch (IOException e) {
-            System.out.println("Считать информацию из файла - невышло. Создался обычный экземпляр FileBackedManager");
-        }
+      //  FileBackedTasksManager newFb = new FileBackedTasksManager(file);
+
+         FileBackedTasksManager  newFb = FileBackedTasksManager.loadFromFile(file);
+
         Task task3 = new Task("Одых", "Украсить ёлку");
         newFb.addNewTaskItem(task3);
         Main.printAllTasksInfo(newFb);
