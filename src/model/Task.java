7262ff00e335292e5234bf1e5cc0a5d565e8a382
DevSMike <likashev.migha@yandex.ru>
2023-01-17 02:38:24 +0300

    feat: добавить возврат null, если статуса нет

--- src/model/Task.java
+++ src/model/Task.java
@@ -3,29 +3,33 @@ package model;
 import service.StatusManager;
 
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
-import java.util.Optional;
 
 public class Task {
 
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yy|HH:mm:ss");
+
     protected int id;
+
     protected String name;
+
     protected String description;
-    protected  String status;
+
+    protected  String status = "";
 
     protected LocalDateTime startTime;
 
     protected LocalDateTime endTime;
 
-    protected long duration;
+    protected Duration duration;
 
 
-    public Task(String name, String description, LocalDateTime startTime, long duration) {
+    public Task(String name, String description, LocalDateTime startTime, Duration duration) {
        this.name = name;
        this.description = description;
        this.startTime = startTime;
@@ -37,7 +41,7 @@ public class Task {
         this.description = description;
     }
 
-    public Task(int id, String name, String status, String description, LocalDateTime startTime, long duration) {
+    public Task(int id, String name, String status, String description, LocalDateTime startTime, Duration duration) {
         this.id = id;
         this.name = name;
         this.status = status;
@@ -69,11 +73,11 @@ public class Task {
         this.endTime = endTime;
     }
 
-    public void setDuration(long duration) {
+    public void setDuration(Duration duration) {
         this.duration = duration;
     }
 
-    public long getDuration() {
+    public Duration getDuration() {
         return duration;
     }
 
@@ -81,7 +85,9 @@ public class Task {
         status = StatusManager.getStatusStringById(id);
     }
     public StatusManager.Statuses getStatus() {
-        return StatusManager.getStatusIdByString(status);
+            if (!status.equals(""))
+                 return StatusManager.getStatusIdByString(status);
+            else return null;
     }
 
     public String getName() {
@@ -122,7 +128,7 @@ public class Task {
 
     public String getInfoWithLocalData() {
         return getId() + "," + getType() + "," + getName() + "," + getStatus() + ","
-                + getDescription() + "," + getDuration() + ","
+                + getDescription() + "," + getDuration()+ ","
                 + getStartTime().format(DATE_TIME_FORMATTER) + "," + getEndTime().format(DATE_TIME_FORMATTER) + ","
                 + getStringEpicId() +"\n";
     }
