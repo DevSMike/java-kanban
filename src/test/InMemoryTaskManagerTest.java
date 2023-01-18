8805a9e9b410ac784140d49c741b99407d2adece
DevSMike <likashev.migha@yandex.ru>
2023-01-18 14:53:50 +0300

    feat: добавить тест на создание хэш мап

--- src/test/InMemoryTaskManagerTest.java
+++ src/test/InMemoryTaskManagerTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.InMemoryTaskManager;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
 
     private final InMemoryTaskManager taskManager = new InMemoryTaskManager();
@@ -11,129 +14,13 @@ class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
     @BeforeEach
     void createNewInMemoryTaskManager() {
         super.setTaskManager(taskManager);
-
-    }
-    @Test
-    void addNewTaskItem() {
-        super.addNewTaskItem();
     }
 
     @Test
-    void addNewEpicItem() {
-        super.addNewEpicItem();
+    void createNewInMemoryTaskManagerShouldCreateThreeEmptyMaps() {
+        InMemoryTaskManager testManager = new InMemoryTaskManager();
+        assertNotNull(testManager);
+        assertTrue(testManager.isMapsCreated());
     }
 
-    @Test
-    void addNewSubtaskItem() {
-       super.addNewSubtaskItem();
-    }
-
-    @Test
-    void getTasks() {
-        super.getTasks();
-    }
-
-    @Test
-    void getEpics() {
-        super.getEpics();
-    }
-
-
-    @Test
-    void getSubtasks() {
-        super.getSubtasks();
-    }
-
-    @Test
-    void deleteTasks() {
-        super.deleteTasks();
-    }
-
-    @Test
-    void deleteEpics() {
-        super.deleteEpics();
-    }
-
-    @Test
-    void deleteSubtasks() {
-        super.deleteSubtasks();
-
-    }
-
-    @Test
-    void getTaskById() {
-        super.getTaskById();
-    }
-
-    @Test
-    void getEpicById() {
-       super.getEpicById();
-    }
-
-    @Test
-    void getSubtaskById() {
-      super.getSubtaskById();
-
-    }
-
-    @Test
-    void updateEpicInfo() {
-        super.updateEpicInfo();
-    }
-
-    @Test
-    void deleteTaskById() {
-        super.deleteTaskById();
-    }
-
-    @Test
-    void deleteEpicById() {
-        super.deleteEpicById();
-    }
-
-    @Test
-    void deleteSubtaskById() {
-        super.deleteSubtaskById();
-    }
-
-    @Test
-    void getSubtaskForEpic() {
-      super.getSubtaskForEpic();
-
-    }
-
-    @Test
-    void updateTask() {
-        super.updateTask();
-    }
-
-    @Test
-    void updateEpic() {
-        super.updateEpic();
-    }
-
-    @Test
-    void updateSubtask() {
-        super.updateSubtask();
-    }
-
-    @Test
-    void setTaskStatus() {
-        super.setTaskStatus();
-    }
-
-    @Test
-    void setSubtaskStatus() {
-        super.setSubtaskStatus();
-    }
-
-    @Test
-    void getHistory() {
-        super.getHistory();
-    }
-
-    @Test
-    void getPrioritizedTasks() {
-        super.getPrioritizedTasks();
-    }
 }
