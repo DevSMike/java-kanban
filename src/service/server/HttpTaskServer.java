7476f9cc5c765dea30c866934e61b51e4b503476
DevSMike <likashev.migha@yandex.ru>
2023-01-27 01:45:56 +0300

    test: реализовать обработку всех эндпоинтов по ТЗ

--- src/service/server/HttpTaskServer.java
+++ src/service/server/HttpTaskServer.java
@@ -1,28 +1,38 @@
-package service;
+package service.server;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import exception.MethodExecutionException;
 import model.Epic;
 import model.Subtask;
 import model.Task;
+import service.Managers;
+import service.TaskManager;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class HttpTaskServer  {
 
     final static private int PORT = 8080;
-    final TaskManager tasksManager = Managers.getDefault();
-    private final Gson gson = new Gson();
-
-    public HttpTaskServer() {
+    private static final String URL = "http://localhost:8078/";
+    TaskManager tasksManager = Managers.getDefault();
+    private final Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new LocalDateAdapter())
+            .registerTypeAdapter(Duration.class, new DurationAdapter()).create();
+    HttpServer httpServer;
+    public HttpTaskServer() throws IOException, InterruptedException {
         try {
-            HttpServer httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
+            tryLoadManager();
+            httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
             httpServer.createContext("/tasks", this::handle);
             httpServer.start();
             System.out.println("Сервер успешно запущен на порту: " + PORT);
@@ -31,39 +41,53 @@ public class HttpTaskServer  {
         }
     }
 
+    //Для тестов
+    public TaskManager getTasksManager() {
+        return  tasksManager;
+    }
 
-    public static void main(String[] args) {
-        HttpTaskServer server = new HttpTaskServer();
+    private void tryLoadManager() {
+        try {
+            tasksManager = HttpTaskManager.loadFromServer(URL);
+        } catch (Exception e) {
+            System.out.println("Это первый вызов manager");
+        }
+    }
+    public void stop() {
+        httpServer.stop(0);
     }
 
 
     public void handle(HttpExchange httpExchange) throws IOException {
         URI uri = httpExchange.getRequestURI();
         String path = uri.getPath();
-        String type = path.split("/")[2];
+        String type = getPathType(path);
         String method = httpExchange.getRequestMethod();
 
         switch (method) {
             case "POST": {
                 if (isUriHasQuery(httpExchange)) {
-                    httpExchange.sendResponseHeaders(400, 0);
-                    try (OutputStream os = httpExchange.getResponseBody()) {
-                        os.write("Данный метод не поддерживается!".getBytes());
-                    }
+                    writeResponse(httpExchange, 400, "Данный метод не поддерживается!");
                     return;
                 }
-                try {
-                    postTask(httpExchange, type);
-                } catch (IOException e ) {
-                    System.out.println("Не получилось создать таску");
-                } finally {
-                    httpExchange.close();
-                }
+                postTask(httpExchange, type);
                 break;
             }
             case "GET": {
+                if (type.isEmpty()) {
+                    getPrioritizedTasks(httpExchange);
+                    return;
+                }
                 if (isUriHasQuery(httpExchange)) {
+                    if (isUriHasEpicId(path)) {
+                        getEpicSubtaskId(httpExchange);
+                        return;
+                    }
                     getTaskById(httpExchange, type);
+                    return;
+                }
+                if (type.equals("history")){
+                    getHistory(httpExchange);
                     return;
                 }
                 getTasks(httpExchange, type);
@@ -79,6 +103,76 @@ public class HttpTaskServer  {
         }
     }
 
+    private String getPathType(String path) {
+        try {
+            return  path.split("/")[2];
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    private void getHistory(HttpExchange httpExchange) {
+        int rCode =200;
+        String response;
+        try {
+            response = gson.toJson(tasksManager.getHistory());
+        } catch (MethodExecutionException e) {
+            response = "Список истории пуст";
+            rCode = 400;
+        }
+        writeResponse(httpExchange, rCode, response);
+    }
+    private void getPrioritizedTasks(HttpExchange httpExchange) {
+        int rCode = 200;
+        String response;
+        try {
+            response = gson.toJson(tasksManager.getPrioritizedTasks());
+        } catch (MethodExecutionException e) {
+            response = "Нет списка приоритетных задач!";
+            rCode = 400;
+        }
+        writeResponse(httpExchange, rCode, response);
+    }
+
+    private void writeResponse(HttpExchange httpExchange, int rCode, String response) {
+        try {
+            httpExchange.sendResponseHeaders(rCode, 0);
+            try (OutputStream os = httpExchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        } catch (IOException e) {
+            System.out.println("Не получилось открыть поток");
+        }
+    }
+
+    private void getEpicSubtaskId(HttpExchange httpExchange) {
+        URI uri = httpExchange.getRequestURI();
+        String path = uri.toString();
+        int id = parsedId(path.substring(path.lastIndexOf("=")+1));
+        int rCode = 200;
+        String response = "";
+        try {
+            if (tasksManager.getEpics().contains(tasksManager.getEpicById(id))) {
+                List<Subtask> subs = tasksManager.getSubtasks().stream()
+                        .filter(subtask -> subtask.getEpicId().equals(tasksManager.getEpicById(id).getId()))
+                        .collect(Collectors.toList());
+                response = gson.toJson(subs);
+            }
+        } catch ( MethodExecutionException e) {
+            rCode = 400;
+            response = "Данного эпика не существует";
+        }
+       writeResponse(httpExchange, rCode, response);
+    }
+
+    private boolean isUriHasEpicId(String path) {
+        try {
+            return path.split("/")[3].equals("epic");
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
     private boolean isUriHasQuery(HttpExchange httpExchange) {
         try {
             return httpExchange.getRequestURI().getQuery().contains("id");
@@ -87,7 +181,7 @@ public class HttpTaskServer  {
         }
     }
 
-    public void getTaskById(HttpExchange httpExchange, String type)  {
+    private void getTaskById(HttpExchange httpExchange, String type)  {
         URI uri = httpExchange.getRequestURI();
         String path = uri.toString();
         int id = parsedId(path.substring(path.lastIndexOf("=")+1));
@@ -113,24 +207,17 @@ public class HttpTaskServer  {
             }
         } catch (MethodExecutionException e) {
             response = "Такого таска не существует";
-            rCode = 400;
+            rCode = 404;
         }
-        try {
-            httpExchange.sendResponseHeaders(rCode, 0);
-            try (OutputStream os = httpExchange.getResponseBody()) {
-                os.write(response.getBytes());
-            }
-        } catch (IOException e) {
-            System.out.println("Невозможно отравить ответ!");
-        }
+        writeResponse(httpExchange, rCode, response);
     }
 
-    public  void deleteTaskById(HttpExchange httpExchange, String type) {
+    private void deleteTaskById(HttpExchange httpExchange, String type) {
         URI uri = httpExchange.getRequestURI();
         String path = uri.toString();
         int id = parsedId(path.substring(path.lastIndexOf("=")+1));
         int rCode = 200;
-        String response;
+        String response = "";
         try {
             switch(type) {
                 case "task": {
@@ -149,19 +236,14 @@ public class HttpTaskServer  {
                     System.out.println("Тип не поддерживается!");
                     rCode = 400;
                 }
+                response = type + " с id:" + id + " успешно удалён!";
             }
-        } catch (MethodExecutionException e) {
-            System.out.println("Такого таска не существует");
+        } catch (Exception e) {
+            System.out.println("Ошибка при удалении таска");
+            rCode = 404;
         }
-        try {
-            response = type + " с id:" + id + " успешно удалён!";
-            httpExchange.sendResponseHeaders(rCode, 0);
-            try (OutputStream os = httpExchange.getResponseBody()) {
-                os.write(response.getBytes());
-            }
-        } catch (IOException e) {
-            System.out.println("Невозможно отравить ответ!");
-        }
+
+        writeResponse(httpExchange, rCode, response);
     }
 
     private int parsedId (String id) {
@@ -194,14 +276,7 @@ public class HttpTaskServer  {
                 rCode = 400;
             }
         }
-        try {
-            httpExchange.sendResponseHeaders(rCode, 0);
-            try (OutputStream os = httpExchange.getResponseBody()) {
-                os.write((type + "s успешно удалены!").getBytes());
-            }
-        } catch (IOException e) {
-            System.out.println("Невозможно отравить ответ!");
-        }
+        writeResponse(httpExchange, rCode,(type + "s успешно удалены!" ));
     }
 
     private void getTasks(HttpExchange httpExchange, String type) {
@@ -213,14 +288,7 @@ public class HttpTaskServer  {
             rCode = 400;
             response = "Вызываемый список - пустой";
         }
-        try {
-            httpExchange.sendResponseHeaders(rCode, 0);
-            try (OutputStream os = httpExchange.getResponseBody()) {
-                os.write(response.getBytes());
-            }
-        } catch (IOException e) {
-            System.out.println("Не получилось получить таски");
-        }
+        writeResponse(httpExchange, rCode, response);
     }
 
     private String getJsonTasks(String type) {
@@ -229,8 +297,8 @@ public class HttpTaskServer  {
             case "task" : {
                 try {
                     json = gson.toJson(tasksManager.getTasks());
-                } catch (MethodExecutionException e) {
-                    System.out.println("Список тасков пустой");
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
                 break;
             }
@@ -246,7 +314,7 @@ public class HttpTaskServer  {
                 try {
                     json = gson.toJson(tasksManager.getSubtasks());
                 } catch (MethodExecutionException e) {
-                    System.out.println("Список сабтасков пустой");
+                    System.out.println("Список subtasks пустой");
                 }
                 break;
             }
@@ -254,9 +322,14 @@ public class HttpTaskServer  {
         return json;
     }
 
-    private void postTask(HttpExchange httpExchange, String type) throws IOException {
+    private void postTask(HttpExchange httpExchange, String type)  {
         InputStream inputStream = httpExchange.getRequestBody();
-        String requestBody = new String(inputStream.readAllBytes());
+        String requestBody = "";
+        try {
+            requestBody = new String(inputStream.readAllBytes());
+        } catch (IOException e) {
+            System.out.println("Не удалось прочитать поток байтов");
+        }
         int rCode;
         String response;
         if (tryToCreateTasks(tasksManager, requestBody, type)) {
@@ -266,10 +339,7 @@ public class HttpTaskServer  {
             rCode = 400;
             response = "Не удалось создать объект";
         }
-        httpExchange.sendResponseHeaders(rCode, 0);
-        try (OutputStream os = httpExchange.getResponseBody()) {
-            os.write(response.getBytes());
-        }
+        writeResponse(httpExchange, rCode, response);
     }
 
     private boolean tryToCreateTasks(TaskManager taskManager, String request, String type)
@@ -291,7 +361,8 @@ public class HttpTaskServer  {
                     break;
                 }
                 case "epic" : {
-                    Epic epic = gson.fromJson(request, Epic.class);
+                    Epic jsonEpic = gson.fromJson(request, Epic.class);
+                    Epic epic = new Epic(jsonEpic.getName(), jsonEpic.getDescription());
                     try {
                         if (taskManager.getEpics().contains(epic)) {
                             taskManager.updateEpic(epic);
@@ -317,13 +388,15 @@ public class HttpTaskServer  {
                     break;
                 } default: {
                     System.out.println("Неверный тип передан!");
+
                     isCouldCreate = false;
                 }
             }
         } catch (Exception e) {
-            System.out.println("Не получилось создать TASK из Json");
+            e.printStackTrace();
             isCouldCreate = false;
         }
         return isCouldCreate;
     }
 }
+
