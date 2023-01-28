89ff7eb6c704138101ad5f4f44b573c43b4fdeaa
DevSMike <likashev.migha@yandex.ru>
2023-01-28 13:17:18 +0300

    feat: добавить Gson файл по умолчанию

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,8 +1,14 @@
 package service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import service.server.DurationAdapter;
 import service.server.HttpTaskManager;
+import service.server.LocalDateAdapter;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class Managers {
 
@@ -14,6 +20,11 @@ public class Managers {
 
     public static HistoryManager getDefaultHistory() {
        return new InMemoryHistoryManager();
+    }
+
+    public static Gson getDefaultGson() {
+        return new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new LocalDateAdapter())
+                .registerTypeAdapter(Duration.class, new DurationAdapter()).create();
     }
 }
 
