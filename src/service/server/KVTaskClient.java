27d23db0076bd84cf55889208ae51ca327f3852c
DevSMike <likashev.migha@yandex.ru>
2023-01-28 13:25:22 +0300

    feat: добавить обработку исключений в каждый метод класса

--- src/service/server/KVTaskClient.java
+++ src/service/server/KVTaskClient.java
@@ -9,28 +9,41 @@ import java.net.http.HttpResponse;
 public class KVTaskClient {
 
     private String apiToken;
-    private String hostUrl;
+    private final String hostUrl;
     private final HttpClient client = HttpClient.newHttpClient();
 
-    public KVTaskClient(String url) throws IOException, InterruptedException {
+    public KVTaskClient(String url)  {
         hostUrl = url;
         URI uri = URI.create(url+"register");
         HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        apiToken = response.body();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            apiToken = response.body();
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Не получилось отправить запрос!");
+        }
     }
 
-    public void put(String key, String json) throws IOException, InterruptedException {
+    public void put(String key, String json)  {
         URI url = URI.create(hostUrl + "save/" + key + "?API_TOKEN=" + apiToken);
         HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(json);
-        HttpRequest request = HttpRequest.newBuilder().uri(url).POST(body).build();
-        client.send(request, HttpResponse.BodyHandlers.ofString());
+        try {
+            HttpRequest request = HttpRequest.newBuilder().uri(url).POST(body).build();
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Не получилось отправить запрос!");
+        }
     }
 
-    public String load(String key) throws IOException, InterruptedException {
+    public String load(String key)  {
         URI uri = URI.create(hostUrl + "load/" + key + "?API_TOKEN=" + apiToken);
-        HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
+        try {
+            HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Не получилось отправить запрос!");
+        }
+        return "error";
     }
 }
